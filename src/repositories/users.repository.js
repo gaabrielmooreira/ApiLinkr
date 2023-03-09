ab540f36d9015dc51f52f5b7bab101519953652d
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-03-09 18:29:47 -0300

    feat: search user by substring

--- src/repositories/users.repository.js
+++ src/repositories/users.repository.js
@@ -8,3 +8,11 @@ export async function insertUser (name, email, password, url){
     db.query(`INSERT INTO users (name, email, password, url) VALUES ($1, $2, $3, $4);`, [name, email, password, url])
     return "ok"
 }
+
+
+export async function searchUserInDB(string) {
+    return await db.query(`
+    SELECT users.id,users.name, users.url AS photo_user from users
+    WHERE LOWER(name) LIKE '%' || LOWER($1) || '%';
+    `,[string])
+}
